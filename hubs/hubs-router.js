df11bcc3d9a1fcb734fe28ea22e9ad179f7082dc
Shafi Masoumi <masoumi2017@icloud.com>
2020-01-21 17:41:30 -0800

    All Gets requests done

--- hubs/hubs-router.js
+++ hubs/hubs-router.js
@@ -46,6 +46,58 @@ router.post('/', (req, res) => {
   })
   
   
+  router.get('/', (req, res) => {
+    Hubs.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(error => {
+      console.log('error on GET /api/posts/', error);
+      res.status(500).json({
+        errorMessage: 'The posts information could not be retrieved.'
+      })
+    })
+  })
+  
+  
+  router.get('/:id', (req, res) => {
+    const id = req.params.id;
+  
+    Hubs.findById(id)
+    .then(post => {
+      if (post.length !== 0) {
+        res.status(200).json(post)
+      } else {
+        res.status(404).json({ errorMessage: 'The post with the specified ID does not exist.' })
+      }
+    })
+    .catch(error => {
+      console.log('error on GET /api/posts/:id', error);
+      res.status(500).json({
+        errorMessage: 'The post information could not be retrieved.'
+      })
+    })
+  })
+  
 
+  router.get('/:id/comments', (req, res) => {
+    const id = req.params.id;
+  
+    Hubs.findPostComments(id)
+    .then(comment => {
+      if (comment.length !== 0) {
+        res.status(200).json(comment)
+      } else {
+        res.status(404).json({ errorMessage: 'The post with the specified ID does not exist.' })
+      }
+    })
+    .catch(error => {
+      console.log('error on GET /api/posts/:id/comments', error)
+      res.status(500).json({
+        errorMessage: 'The comments information could not be retrieved.'
+      })
+    })
+  })
+  
 module.exports = router;
 
